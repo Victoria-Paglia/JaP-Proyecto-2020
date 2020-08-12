0f698264f31166b0d0bf01d0eb070f24a26759c1
Victoria <victoriapaglia2018@gmail.com>
2020-08-12 14:07:08 -0300

    Entrega 1

--- js/login.js
+++ js/login.js
@@ -2,5 +2,32 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("enviar").addEventListener("click", function(e) {
+        let ingEmail = document.getElementById("ingEmail");
+        let ingContraseña = document.getElementById("ingContraseña");
+        let datosCompletos = true;
 
+        if (ingEmail.value === "") {
+            ingEmail.classList.add("invalid");
+            datosCompletos = false;
+        }
+
+        if (ingContraseña.value === "") {
+            ingContraseña.classList.add("invalid");
+            datosCompletos = false;
+        }
+
+        if (datosCompletos) {
+
+            if (ingEmail.value, ingContraseña.value) {
+                window.location = 'index.html';
+            }
+            else {
+                alert("Usuario o contraseña incorrectas!")
+            }
+        }
+        else {
+            alert("Debes ingresar los datos!")
+        }
+    });
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,47 @@
+var productsArray = [];
+
+function showProductsList(array) {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < array.length; i++) {
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+        
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <div class="mb-1">    
+                        <h4>`+ product.name + `</h4>
+                        <p >`+ product.description + `</p>
+                        <p>`+ product.cost + `</p>
+                        <p>`+ product.currency + `</p>
+                        </div>
+                    <small class="text-muted">` + product.soldCount + ` artículos</small>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("mod-1").innerHTML = htmlContentToAppend;
+    }
+}   
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productsArray = resultObj.data;
+            showProductsList(productsArray);
+        }
+    });
 });
